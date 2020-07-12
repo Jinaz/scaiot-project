3e8d3e79bb465ef24bb7875efcc55a4c7e26ac13
Jinaz <st141130@stud.uni-stuttgart.de>
2020-07-12 16:29:50 +0200

    decider updated

--- Implementation/IOTHubActuator/decider.py
+++ Implementation/IOTHubActuator/decider.py
@@ -26,13 +26,13 @@ def generate_content(actions, room):
             content += str(count) + ". " + "please open the windows\n"
             ex.open_Windows()
             room.window.status = 1
-        if action ==  'close_Windows':
+        if action == 'close_Windows':
             content += str(count) + ". " + "please close the windows\n"
             ex.close_Windows()
             room.window.status = 0
-        if action ==  'nothing':
+        if action == 'nothing':
             ex.nothing()
-        if action ==  'refresh_air':
+        if action == 'refresh_air':
             content += str(count) + ". " + "please open the windows between lectures (recommended)\n"
             ex.refresh_air()
             room.window.status = 1
@@ -40,15 +40,15 @@ def generate_content(actions, room):
             content += str(count) + ". " + "please turn up the lights a little\n"
             ex.dimLightsup()
             room.light.status = 1
-        if action ==  'dimLightsdown':
+        if action == 'dimLightsdown':
             content += str(count) + ". " + "please dim the lights\n"
             ex.dimLightsdown()
             room.light.status = 1
-        if action ==  'fullLights':
+        if action == 'fullLights':
             content += str(count) + ". " + "please turn on the lights\n"
             ex.fullLights()
             room.light.status = 2
-        if action ==  'lightsOff':
+        if action == 'lightsOff':
             content += str(count) + ". " + "please turn off the lights\n"
             ex.lightsOff()
             room.light.status = 0
@@ -56,15 +56,15 @@ def generate_content(actions, room):
             content += str(count) + ". " + "please open the curtain\n"
             ex.curtain_up()
             room.curtain.status = 0
-        if action ==  'curtain_down':
+        if action == 'curtain_down':
             content += str(count) + ". " + "please close the curtain\n"
             ex.curtain_down()
             room.curtain.status = 1
-        if action ==  'unlockRoom':
+        if action == 'unlockRoom':
             content += str(count) + ". " + "please open the lecture room\n"
             ex.unlockRoom()
             room.door.status = 0
-        if action ==  'lockRoom':
+        if action == 'lockDoor':
             content += str(count) + ". " + "please close the lecture room\n"
             ex.lockRoom()
             room.door.status = 1
